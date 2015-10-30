c44a3adc48f6acd55c3a32e4f1e1dc95c2e853cd
MarkNewcomb <marknewcomb@Marks-MacBook-Pro.local>
2015-10-30 16:16:41 -0600

    reorganized form, added Morgan's notes

--- form.php
+++ form.php
@@ -138,18 +138,27 @@ if ( ! empty( $_POST ) ) {
   $mysqli->close();
 }
 ?>
+
+<!-- Current database model is fine, nothing too complicated -->
 <form method="post" action="thankyou.php" class="flora">
 	<h1>Flora Finder<span>Please fill all the text in the fields.</span></h1>
+	 <label for="person-name"><span>Enter your name:</span></label>
+ <input name="person-name" type="text">
+	<!--   KEEP plant name a text box -->
 <label for="plant-name"><span>Plant Name (Enter the name of the Plant, if known. Otherwise enter a brief description of the Plant.)</span></label>
   <input name="plant-name" type="text">
+<!--   KEEP soil conditions a text box -->
  <label for="soil-conditions"><span>Soil Conditions (Enter any combination of Clay, Loam, Peat, Rocky, Sand, or Silt descriptors as well as Soil Color.)</span></label>
  <input name="soil-conditions" type="text">
+<!--  weather api; but also give user ability to manually override ( she would take user-submitted data preference over API-generated) but only have one record, don't have both records in there. If you can get weather api working, populate that field with it. BUT, if the user checks a box to manually override this and enter in their own thing, then THAT field is put into the record) get temperature, high/low, description -->
  <label for="weather-conditions"><span>Weather Conditions (Enter the weather conditions while observing the Plant. Enter any combination of Sunny, Partly Sunny, Cloudy, Raining, Snowing, Fog, Misting, Windy, etc. Also enter an estimate of the Temperature.)</span></label>
- <input name="weather-conditions" type="text">
+ <input name="weather-conditions" type="text"> 
+<!--  HAVE a date/time field, should be generated. give user ability to override date/time in case there's no connectivity with the phone. -->
+<label for="datetime"><span>Date/Time</span></label>
+<input name="datetime" type="text">
+<!--  geolocation; have to get user permission to allow for that, but if you CAN get the geolocation that's great, but also make it possible for someone to input the latitude and longitude if they wish (try to validate it); if they don't know THAT, then just a text box for location (make at least one required if the other two fail) -->
  <label for="location"><span>Location (Enter the location where the Plant was seen.)</span></label>
  <input name="location" type="text">
- <label for="person-name"><span>Enter your name:</span></label>
- <input name="person-name" type="text">
  <label for="notes"><span>Additional Notes (Add any additional notes and observations.)</span></label>
  <input name="notes" type="text">
   <input type="submit" value="Submit Form" class="button"><br>
